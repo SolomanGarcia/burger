da50790664d037bb1a79e3775c644070eda40c38
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-16 12:19:58 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Hangover Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Mushroom Swiss Cheeseburger', false);
