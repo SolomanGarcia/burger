6ca82b1bd2e92863d1f934ef5750ed912d7eeb9d
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-17 08:49:22 -0600

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,5 +9,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP not null DEFAULT current_timestamp,
     PRIMARY KEY(id)
 );
